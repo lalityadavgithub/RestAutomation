bd8d40bc9e3b6777c120ef89f3d9984271d63957
KUNAL <KUNAL@host.docker.internal>
2026-01-29 18:43:35 +0530

    add 29 jan 2026

--- RestAutomation/src/test/java/API/RestAutomation/ComplexJsonParse.java
+++ RestAutomation/src/test/java/API/RestAutomation/ComplexJsonParse.java
@@ -6,7 +6,10 @@ import io.restassured.path.json.JsonPath;
 public class ComplexJsonParse {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		
+		int allPrice=0;
 		
 		JsonPath js=new JsonPath(payload.CoursePrice());
 		
@@ -28,12 +31,52 @@ public class ComplexJsonParse {
 			
 			String titles=js.get("courses["+i+"].title");
 			int price=js.getInt("courses["+i+"].price");
+			//int copies=js.getInt("courses["+i+"].copies");
+
 			
 			System.out.println("title: " + titles);
 			System.out.println("price: " + price);
-			System.out.println();
+		//	System.out.println("copies: "+ copies );
 			
 		}
+		
+		System.out.println("Print no of copies sold bt RPA Courses");
+		
+	for(int i=0; i<count; i++) {
+			
+		String titles=js.get("courses["+i+"].title");
+		
+		if(titles.equalsIgnoreCase("RPA")) {
+			
+			
+			int copies=js.getInt("courses["+i+"].copies");
+			
+			System.out.println("copies: " + copies);
+			break;
+		}
+		}
+	
+	/* System.out.println("Verify if Sum of all Courses prices matches with Purchase Amount");
+	
+	for(int i=0; i<count; i++) {
+		
+	
+			int prices=js.getInt("courses["+i+"].price");
+			
+			allPrice=allPrice +prices;
+			
+		}
+	System.out.println("TotalAmount: " + allPrice);
+	
+	if(allPrice==totalAmount) {
+		
+		System.out.println("Sum of all Courses prices matches with Purchase Amount");
+	}else {
+		System.out.println("Not Matching");
+
+	}
+	*/
+	
 	}
 
 }
---
+++ RestAutomation/src/test/java/API/RestAutomation/SumValidation.java
@@ -0,0 +1,35 @@
+package API.RestAutomation;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import files.payload;
+import io.restassured.path.json.JsonPath;
+
+public class SumValidation {
+	
+	@Test
+	public void sumOfCourses() {
+		
+		int sum =0;
+		JsonPath js=new JsonPath(payload.CoursePrice());
+		int count = js.getInt("courses.size()");
+		for(int i=0; i<count; i++) {
+			
+			int prices=js.get("courses["+i+"].price");
+			int copies=js.get("courses["+i+"].copies");
+			int amount=prices * copies;
+			
+			System.out.println(amount);
+			
+			sum=sum+amount;
+			
+		}
+		
+		System.out.println(sum);
+		
+		int  purchaseAmount=js.getInt("dashboard.purchaseAmount");
+		Assert.assertEquals(sum, purchaseAmount);
+	}
+
+}
